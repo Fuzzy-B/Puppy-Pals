58af3c10755bcde371cf5bf516ac3327d2b1a5a4
Ian Balderston <ianmb@vt.edu>
2024-02-20 10:52:22 -0500

    added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ import { useState } from 'react'
 
 function App() { 
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log('puppies: ', puppies)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+  
   return ( 
       <div className='App'>
+        {featPupId && (
+        <div class='featPup'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          }) 
+        } 
+
+         
+
       </div>
      
   )
